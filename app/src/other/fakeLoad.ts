f9700b2e617ce5b9529781f1fe9e89ff6c5ecaed
Mufazzal <h.mufazzal@gmail.com>
2022-06-13 11:17:04 +0530

    sdf

--- app/src/other/fakeLoad.ts
+++ app/src/other/fakeLoad.ts
@@ -12,22 +12,22 @@ const fakeLoad_1 = function() {
         const v9 = 123699988234566*887699954 
     }
 
-    const now = new Date().getTime();
+    const now = new Date().getTime()
     const tt = (now - time) / 1000 
-    return `at: ${time} took ${tt} seconds`;
+    return `at: ${time} took ${tt} seconds`
 
 }
 
 
 function fakeLoad(baseNumber) {
     const time = new Date().getTime()
-	let result = 0;	
-	for (var i = Math.pow(baseNumber, 7); i >= 0; i--) {		
-		result += Math.atan(i) * Math.tan(i);
-	};
-    const now = new Date().getTime();
+	let result = 0	
+	for (let i = Math.pow(baseNumber, 7); i >= 0; i--) {		
+		result += Math.atan(i) * Math.tan(i)
+	}
+    const now = new Date().getTime()
     const tt = (now - time) / 1000 
-    return `at: ${time} took ${tt} seconds`;}
+    return `at: ${time} took ${tt} seconds`}
 
 
 export {fakeLoad}
