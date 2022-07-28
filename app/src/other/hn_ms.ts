39517213515713f0027a760c29761b6b8dbb2706
Mufazzal <h.mufazzal@gmail.com>
2022-07-29 00:23:16 +0530

    Update hn_ms.ts

--- app/src/other/hn_ms.ts
+++ app/src/other/hn_ms.ts
@@ -16,34 +16,34 @@ async function callHnMsFlow1() {
 
     const out: any = {'flow': 'hn-ms-1 | hn-ms-2 | hn-ms-3'}
 logger.info(out)    
-    out.hn_ms1 = await fetch(hn_ms_1)
-    .then(res=> {
-        logger.info(res.toString())
-        return res.json()
-    }).then(res => {
-        logger.info(res.toString())
-        return {success: true, res}
-    }).catch(e => {
-        logger.info(e.toString())
-        return {success: false, e}
-    })
+    // out.hn_ms1 = await fetch(hn_ms_1)
+    // .then(res=> {
+    //     logger.info(res.toString())
+    //     return res.json()
+    // }).then(res => {
+    //     logger.info(res.toString())
+    //     return {success: true, res}
+    // }).catch(e => {
+    //     logger.info(e.toString())
+    //     return {success: false, e}
+    // })
 logger.info(out)    
     
-    out.hn_ms2 = await fetch(hn_ms_2)
-    .then(res=> res.json())
-    .then(res => {
-        return {success: true, res}
-    }).catch(e => {
-        return {success: false, e}
-    })  
+    // out.hn_ms2 = await fetch(hn_ms_2)
+    // .then(res=> res.json())
+    // .then(res => {
+    //     return {success: true, res}
+    // }).catch(e => {
+    //     return {success: false, e}
+    // })  
     
-    out.hn_ms3 = await fetch(hn_ms_3)
-    .then(res=> res.json())
-    .then(res => {
-        return {success: true, res}
-    }).catch(e => {
-        return {success: false, e}
-    })      
+    // out.hn_ms3 = await fetch(hn_ms_3)
+    // .then(res=> res.json())
+    // .then(res => {
+    //     return {success: true, res}
+    // }).catch(e => {
+    //     return {success: false, e}
+    // })      
     
     return out
 }
